b7954dc7d2d43b772fa4a82ea62f79d2c6c3fa40
hawaii <137668355@qq.com>
2021-09-21 12:20:57 +0800

    修复服务器61000错误，添加备用线路检测

--- scripts/bilibili_bypass.py
+++ scripts/bilibili_bypass.py
@@ -40,7 +40,7 @@ class BilibiliBypass:
             print('Blocked: ' + flow.request.url)
     
     def response(self, flow: mitmproxy.http.HTTPFlow):
-        if flow.request.url.startswith(api['biligame_login_v3']):
+        if flow.request.url in (api['biligame_l1_login_v3'], api['biligame_l3_login_v3']):
             print('URL: ' + flow.request.url)
             resjson = flow.response.json()
             print('Original response:')
@@ -71,27 +71,24 @@ class BilibiliBypass:
                     # TODO add OS X support
         elif flow.request.url.startswith(api['app_3rd_auth']):
             # Record app's access token
-            if not self.disposable_access_token:
-                self.disposable_access_token = flow.request.urlencoded_form['access_token']
-        elif flow.request.url in (api['biligame_token_login_v3'], api['biligame_token_oauth_login_v3']):
+            # Make sure we always use Bilibili app's token rather than
+            # the temporary token we got from this api
+            # otherwise myinfo api will fail with code=61000
+            self.disposable_access_token = flow.request.urlencoded_form['access_token']
+            print('access_token=' + self.disposable_access_token)
+        elif flow.request.url in (
+            api['biligame_l1_token_login_v3'],
+            api['biligame_l1_token_oauth_login_v3'],
+            api['biligame_l1_token_exchange_v3'],
+            api['biligame_l3_token_login_v3'],
+            api['biligame_l3_token_oauth_login_v3'],
+            api['biligame_l3_token_exchange_v3']
+            ):
             print('URL: ' + flow.request.url)
             resjson = flow.response.json()
             print('Original response:')
             print(resjson)
             if int(resjson['code']) == 500051:
-                self.disposable_access_token = flow.request.urlencoded_form['access_key']
-                self.fill_user_info(resjson, self.disposable_access_token)
-                print('Manipulated response:')
-                print(resjson)
-                flow.response.set_text(json.dumps(resjson))
-                self.clear_token()
-        elif flow.request.url.startswith(api['biligame_token_exchange_v3']):
-            print('URL: ' + flow.request.url)
-            resjson = flow.response.json()
-            print('Original response:')
-            print(resjson)
-            if int(resjson['code']) == 500051:
-                # use Bilibili's access key instead
                 if not self.disposable_access_token:
                     self.disposable_access_token = flow.request.urlencoded_form['access_key']
                 self.fill_user_info(resjson, self.disposable_access_token)
